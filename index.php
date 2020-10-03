74291eefe1867fc186abd68e3ffc3d605e276af3
Kaysier De los Reyes <69897182+kyzier23@users.noreply.github.com>
2020-10-03 23:11:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,89 +4,25 @@
 		<link rel="stylesheet" type = "text/css" href = "css/style.css"/>
 	</head>
 	<body>
-		<div class = "header">
-				<div class="k"></div>
-				<div class="d"></div>
-				<div class="a"></div>
-			<div class = "container20">
-				<div class = "steam steam-1"></div>
-				<div class = "steam steam-2"></div>
-				<div class = "steam steam-3"></div>
-				<div class = "steam steam-4"></div>
-				<div class = "coffe-cup"></div>
-				<div class = "saucer"></div>
-			</div>
-		</div>
-		<div class = "body1">
-			<h1 align = "center"> CHOOSE YOUR TASTE!</h1>
-			<div class="container">
-				<div class="bar">
-					<div class="bold"><h3>Bold</h3></div>
-					<div class="circle top"></div>
-					<div class="circle middle"></div>
-					<div class="circle bottom"></div>
-					<div class="creamy"><h3>Creamy</h3></div>
-				</div>
-				<div class="coffee-cups">
-					<div class="cup-small doppio">
-						<div class="handle"></div>
-					</div>
-					<div class="cup americano">
-						<div class="handle"></div>
-					</div>
-					<div class="cup-small macchiato">
-						<div class="handle"></div>
-					</div>
-					<div class="cup-small flatwhite">
-						<div class="handle"></div>
-					</div>
-					<div class="cup cappuccino">
-						<div class="handle"></div>
-					</div>
-					<div class="cup-long latte">
-						<div class="handle"></div>
-						<div class="bottom-handle"></div>
-					</div>
-					<div class="info">
-    <div class="first">
-      <h1>doppio</h1>
-      <p>Double shot of espresso. Straight.</p>
-    </div>
-    <div class="second">
-      <h1>americano</h1>
-      <p>Shots of espresso diluted with water</p>
-    </div>
-    <div class="third">
-      <h1>Macchiato</h1>
-      <p>Espresso with a dash of frothy foamed milk.</p>
-    </div>
-    <div class="fourth">
-      <h1>Flat White</h1>
-      <p>Double shot of espresso with steamed milk.</p>
-    </div>
-    <div class="fifth">
-      <h1>Cappuccino</h1>
-      <p>Double shot of espresso topped with an airy thick layer of foamed milk.</p>
-    </div>
-    <div class="sixth">
-      <h1>Caffe Latte</h1>
-      <p>A shot of espresso in steamed milk lightly topped with foam.</p>
-    </div>
-  </div>
+		<body>
+  <div class = "header">
+    <div class="k"></div>
+<div class="d"></div>
+<div class="a"></div>
+        <div class = "container10">
+            <div class="coffee-cont10">
+  <div class="coffee-cup10">
+   </div>
+  <div class="smoke10"><div/>
+    <div class="smallsmoke10"></div>
+    <div class="coffee-hand10"></div>
 </div>
+          </div>
+        </div>
+  </div>
 
-<div class="key">
-  <div class="circle espresso"><p class="small">espresso</p></div>
-  <div class="circle milk"><p class="small">steamed milk</p></div>
-  <div class="circle foam"><p class="small">milk foam</p></div>
-  <div class="circle water"><p class="small">water</p></div>
-</div>  
-			<div class = "clear">
-			</div>  <br>
-				<br>
-				</div>
-			</div>
-	<div class = "body2">
+ </div>
+  <div class = "body2">
   <div class="container2">
 
 	<div class="coffee-header1">
@@ -110,25 +46,29 @@
     <br>
     <br>
 <h1 align = "center"> ABOUT US</h1>
-    <br>
     <p align = "center"><font size = "4">The coffee shop business provides coffee connoisseurs with a wide array of choices that satisfy discriminating palates, while providing prompt and courteous service. From growing to harvesting coffee, from roasting to brewing, from serving to keeping customers coming back for more – you need to strive for excellence.
 			</font></p>
-    <br>
  <p align = "center"><font size = "4">And the music you hear in store is chosen for its artistry and appeal.It’s not unusual to see people coming to CAC to chat, meet up or even work. We’re a neighborhood gathering place, a part of the daily routine – and we cou
 			</font></p>
-    <br>
-    <p align = "center"><font size = "4">Here in CAC, we can assure you that we are true to our name. You can enjoy and drink your Coffee And Chill without any worries.
-			</font></p>
-    <div class = "container10">
-            <div class="coffee-cont10">
-  <div class="coffee-cup10">
-   </div>
-  <div class="smoke10"><div/>
-    <div class="smallsmoke10"></div>
-    <div class="coffee-hand10"></div>
+      <div class = "container20">
+       <div class = "steam steam-1"></div>
+      <div class = "steam steam-2"></div>
+      <div class = "steam steam-3"></div>
+      <div class = "steam steam-4"></div>
+      <div class = "coffe-cup"></div>
+        <div class = "saucer"></div>
+      </div>
+    <div class = "container30">
+<div class="coffee">
+	<span class="steam"></span>
+		<div class="coffee-bg-lid"></div>
+		<div class="coffee-rim"></div>
+		<div class="coffee-rim-bt"></div>		
+		<div class="coffee-color"></div>	
+		<div class="coffee-cup"></div>
+		<div class="coffee-shadow"></div>
 </div>
-          </div>
-        </div>
+</div>
     			<div class = "clear"></div>
   </div>
   <div class = "footer">
@@ -149,5 +89,7 @@
 			<p><font size = "2">© 2020 CAC Coffee Company. All rights reserved. </font></p>
     </div>
   </div>
+</div> 
+
 	</body>
 </html>
